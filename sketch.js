0317fc8852552ac6ac23bbf74d1d1905a3fd85ca
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-02-29 11:51:03 -0600

    started

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// AI Character
+// Dyllan Mouly
+// 19 Feb 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(width/2, height/2, 100);
+}
