f9a34268288fdfdc65be24115eb10e1e3ed7b7cc
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-03-05 12:06:56 -0600

    stickman drawing
    the guy steve

--- sketch.js
+++ sketch.js
@@ -12,5 +12,15 @@ function setup() {
 
 function draw() {
   background(220);
-  circle(width/2, height/2, 100);
+  line(75,50,75,150) //body
+  rect(50,50,50,50) //head
+  line(50,120,100,120) //arms
+  line(50,170,75,150) //left leg
+  line(100,170,75,150) //right leg
+  circle(60,65,10) // left eye
+  circle(90,65,10) // right eye
+  line(60,80,100,90) // mouth
+alert("this is steve, say hi to him")
+
 }
+
