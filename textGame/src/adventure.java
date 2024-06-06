44e326a7a5bf43edc29ad96bfabfc8965925a186
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-06-06 11:52:32 -0600

    updates
    two thinihrs

--- textGame/src/adventure.java
+++ textGame/src/adventure.java
@@ -1,3 +1,16 @@
-public static int kingsCall(){
-    System.out.println();
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class adventure {
+    
+
+    public static int kingsCall(){
+        Scanner keys = new Scanner(System.in);
+        System.out.println("You set out towards the King's castle. [Enter]");
+        keys.nextLine();
+        System.out.println("You arrive at the castle and open the giant double doors that separate you from the throne room. [Enter]");
+        keys.nextLine();
+        return 0;
+        
+    }
 }
