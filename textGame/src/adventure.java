7dd58e6879049657fabc3e67ca0e89fc56afc876
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-06-07 11:30:20 -0600

    updatre t

--- textGame/src/adventure.java
+++ textGame/src/adventure.java
@@ -10,10 +10,21 @@ public class adventure {
         keys.nextLine();
         System.out.println("You arrive at the castle and open the giant double doors that separate you from the throne room. [Enter]");
         keys.nextLine();
+        System.out.println("King: 'Hello adventurer!' \n      'I have called you here because there is a great evil in our lands.' \n      'This evil is a powerful dragon who has awoken recently.' \n      'You will have to find the 3 legendary warrior pieces to be able to defeat him.' \n      'However, and these pieces are scattered across the land.' [Enter]");
+        keys.nextLine();
+        System.out.println("King: 'You need to find the Legendary Sword of Flame, hidden in the volcano up north.' \n      'The Shield of Water, hidden in the deep lake to the east.' \n      'And the Rock Armor, hidden in the desert to the west.' [Enter]");
+        keys.nextLine();
+        System.out.println("King: 'The Dragon's Lair is to the south, you'll have to go there after collecting all the warrior pieces.' \n      'I wish you luck brave warrior, please save our kingdom!' [Enter]");
+        keys.nextLine();
+        System.out.println("After the King's yap sesh, you leave the castle in search for the warrior pieces. [Enter]");
+        keys.nextLine();
+        splitpaths();
         return 0;
-        
     }
 
+    public static int splitpaths(){
+        return 0;
+    }
 
     public void beforedragonFight()
     {
@@ -24,6 +35,7 @@ public class adventure {
         keys.nextLine();
         System.out.println("Very well, you prepare yourself as you open death doors of the dragon. From here on out, you feel courageous, a warrior who will save the Kingdom of Pernk. \n You feel Dragon-Hearted.");
     }
+    
     public void dragonFight()
     {
         Scanner keys = new Scanner(System.in);
@@ -31,6 +43,5 @@ public class adventure {
             System.out.println("You enter the Dragon's Den and begun your battle. You took hard hits and did the same to the dragon. \n You moved swiftly to dodge the dragons attack and attacked it from weak areas. \n Finally, the dragon was heavily injured and tired and so were you, so you took this one chance and. . . \n Slayed the dragon ocne and for all!");
 
         }
-
     }
 }
